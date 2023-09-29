9bdbb4cf9e2492c31e3381a47ae459d0b5d11a38
judy-chelangat <judysigilai75@gmail.com>
2023-09-29 20:09:40 +0300

    changed the id column to integer

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class Hero(db.Model):
     __tablename__ = 'hero'
 
-    id = db.Column(db.String(255), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String,nullable=False)
     super_name=db.Column(db.String)
     created_at=db.Column(db.DateTime,server_default=db.func.now())
@@ -18,7 +18,7 @@ class Hero(db.Model):
     
 class HeroPowers(db.Model):
     __tablename='heropowers'
-    id = db.Column(db.String(255), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     strength=db.Column(db.String,nullable=False)
     hero_id=db.Column(db.Integer,db.ForeignKey('hero.id'))
     power_id=db.Column(db.Integer,db.ForeignKey('power.id'))
@@ -29,13 +29,13 @@ class HeroPowers(db.Model):
     hero=db.relationship('Hero',backref='heropowers')
     power=db.relationship('Power',backref='heropowers')
 
-    
+
     def __repr__(self):
         return f'<Heropowers {self.strength} >'
     
 class Power(db.Model):
     __tablename__='power'
-    id = db.Column(db.String(255), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String,nullable=False)
     description=db.Column(db.String)
     created_at=db.Column(db.DateTime,server_default=db.func.now())
