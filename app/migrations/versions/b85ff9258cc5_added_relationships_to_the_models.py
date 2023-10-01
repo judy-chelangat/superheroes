f1190b1347e31de40af33a04dfc27e2e875997f4
judy-chelangat <judysigilai75@gmail.com>
2023-10-01 14:27:29 +0300

    generated migrations

---
+++ app/migrations/versions/b85ff9258cc5_added_relationships_to_the_models.py
@@ -0,0 +1,24 @@
+"""added relationships to the models
+
+Revision ID: b85ff9258cc5
+Revises: a194188fe808
+Create Date: 2023-10-01 11:34:50.999030
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b85ff9258cc5'
+down_revision = 'a194188fe808'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
